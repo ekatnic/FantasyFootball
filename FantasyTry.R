f7ff9fcde0b51c7da054047e24928ecc22bef570
Ethan Katnic <ethankatnic@Ethans-MBP-2.hsd1.wa.comcast.net>
2018-07-07 18:15:13 -0700

    Built initial frame

--- FantasyTry.R
+++ FantasyTry.R
@@ -6,13 +6,23 @@ library(knitr)
 library(DT)
 library(tibble)
 
-url = "http://games.espn.com/ffl/api/v2/scoreboard?leagueId=17056&seasonId=2017&matchupPeriodId=1"
-raw.result <- GET(url = url)
-this.raw.content <- rawToChar(raw.result$content)
+url = "http://games.espn.com/ffl/api/v2/leagueSettings?leagueId=17056&seasonId=2017"
+result <- GET(url = url)
+raw.content <- rawToChar(result$content)
+JSON.content <- fromJSON(this.raw.content)
 
 
-this.content <- fromJSON(this.raw.content)
+awayScores = unlist(JSON.content$leaguesettings$teams$`1`$scheduleItems$matchups[1][[1]]["awayTeamScores"]$awayTeamScores)
+awayTeamName = paste(JSON.content$leaguesettings$teams$`1`$scheduleItems$matchups[1][[1]]$awayTeam$teamLocation, 
+                     JSON.content$leaguesettings$teams$`1`$scheduleItems$matchups[1][[1]]$awayTeam$teamNickname)
+homeScores = unlist(JSON.content$leaguesettings$teams$`1`$scheduleItems$matchups[1][[1]]["homeTeamScores"]$homeTeamScores)
+homeTeamName = paste(JSON.content$leaguesettings$teams$`1`$scheduleItems$matchups[1][[1]]$homeTeam$teamLocation, 
+                     JSON.content$leaguesettings$teams$`1`$scheduleItems$matchups[1][[1]]$homeTeam$teamNickname)
+scoreBoards = data.frame("Home" = homeTeamName, "Away" = awayTeamName, "HomeScore" = homeScores,
+                         "AwayScore" = awayScores, "Week" = 1)
+
+
+
 # flat.content <- flatten(this.content)
-
 
 str(this.content)
