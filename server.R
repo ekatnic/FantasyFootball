b30ea08471db2aecbf4b58e0d76e0f88cdeb20f5
Ethan Katnic <ethankatnic@Ethans-MBP-2.hsd1.wa.comcast.net>
2018-08-07 23:42:11 -0700

    Fixed QB/DST, add autofull, change theme and layout

--- server.R
+++ server.R
@@ -5,12 +5,13 @@ library(tidyverse)
 library(DT)
 library(magrittr)
 
-projectionTable <- read.csv("Data/ProjectionTable.csv", stringsAsFactors = FALSE)
+
+projectionTable <- read.csv("data/ProjectionTable.csv", stringsAsFactors = FALSE)
 
 server <- function(input,output){
   
   output$table <- renderDataTable({
-    playerList <- list(input$QB, input$RB1, input$RB2, input$WR1, input$WR2, input$WR3, input$TE, input$DST, input$K)
+    playerList <- list(input$QB, input$RB1, input$RB2, input$WR1, input$WR2, input$WR3, input$FLEX, input$TE, input$DST, input$K)
     projectionTable <- projectionTable %>% filter(Player %in% playerList) %>% select(-1)
     #---build summary row
     summaryRow <- projectionTable %>% select(c(-1,-2)) %>% summarise_all(funs(sum(.,na.rm = TRUE)))
@@ -20,10 +21,13 @@ server <- function(input,output){
     fullTable <- rbind(projectionTable, summaryRow)
     #---bold summary row
     datatable(fullTable) %>%formatStyle(
-      0,
-      target = "row",
-      fontWeight = styleEqual(nrow(fullTable), "bold")
-    )
-    
+                                        0,
+                                        target = "row",
+                                        fontWeight = styleEqual(nrow(fullTable), "bold")
+                          ) %>% 
+                          formatStyle(c(1:18),
+                                        color = "black"
+                          )
+  
   })
 }
--- ui.r
+++ ui.r
@@ -1,37 +1,58 @@
 library(shiny)
 source("server.R")
 library("markdown")
+library(shinythemes)
 
-ui<- fluidPage(
-  titlePanel("List of Players"),
-  textInput(inputId = "QB",
-            label = "Quarterback", 
-            value = ""),
-  textInput(inputId = "RB1",
-            label = "Running Back 1", 
-            value = ""),
-  textInput(inputId = "RB2",
-            label = "Runinng Back 2", 
-            value = ""),
-  textInput(inputId = "WR1",
-            label = "Wide Receiver 1", 
-            value = ""),
-  textInput(inputId = "WR2",
-            label = "Wide Receiver 2", 
-            value = ""),
-  textInput(inputId = "WR3",
-            label = "Wide Receiver 3", 
-            value = ""),
-  textInput(inputId = "TE",
-            label = "Tight End", 
-            value = ""),
-  textInput(inputId = "DST",
-            label = "Defense/Special Teams", 
-            value = ""),
-  textInput(inputId = "K",
-            label = "Kicker", 
-            value = ""),
-  fluidRow(
-    dataTableOutput(outputId = "table")
-  )
-)
+ui<- shinyUI(fluidPage(theme = shinytheme("superhero"),
+                  titlePanel("Players:"),
+                  fluidRow(column(3,
+                                  selectizeInput(inputId = "QB",
+                                          label = "Quarterback", 
+                                          choices = (projectionTable %>% filter(Position == "QB"))$Player,
+                                          selected = ""
+                                                 ),
+                                  selectizeInput(inputId = "RB1",
+                                          label = "Running Back 1", 
+                                          choices = (projectionTable %>% filter(Position == "RB"))$Player
+                                                ),
+                                  selectizeInput(inputId = "RB2",
+                                          label = "Running Back 2", 
+                                          choices = (projectionTable %>% filter(Position == "RB"))$Player
+                                                ),
+                                  selectizeInput(inputId = "TE",
+                                                 label = "Tight End", 
+                                                 choices = (projectionTable %>% filter(Position == "TE"))$Player
+                                  ),
+                                  selectizeInput(inputId = "FLEX",
+                                                 label = "Flex", 
+                                                 choices = (projectionTable %>% filter(Position %in% c("RB","WR","TE")))$Player
+                                  )
+                                )
+                      , column(3, offset = 2,
+                            selectizeInput(inputId = "WR1",
+                                              label = "Wide Receiver 1", 
+                                              choices = (projectionTable %>% filter(Position == "WR"))$Player
+                            ),
+                               selectizeInput(inputId = "WR2",
+                                              label = "Wide Receiver 2", 
+                                              choices = (projectionTable %>% filter(Position == "WR"))$Player
+                            ),
+                            selectizeInput(inputId = "WR3",
+                                 label = "Wide Receiver 3", 
+                                 choices = (projectionTable %>% filter(Position == "WR"))$Player
+                            ),
+                            selectizeInput(inputId = "DST",
+                                           label = "Defense/Special Teams", 
+                                           choices = (projectionTable %>% filter(Position == "DST"))$Player
+                            ),
+                            selectizeInput(inputId = "K",
+                                           label = "Kicker", 
+                                           choices = (projectionTable %>% filter(Position == "K"))$Player
+                            )
+                      )
+                  ),
+                  fluidRow( 
+                    dataTableOutput(outputId = "table")
+                  )
+              )
+        )
